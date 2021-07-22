1e3e2450fb7fc77f62b6ac22c10741df6a897961
andyarensman <86622025+andyarensman@users.noreply.github.com>
2021-07-22 10:42:12 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var cors = require('cors');
+var multer = require('multer')
+require('dotenv').config()
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+});
+
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
+
+
+app.post('/api/fileanalyse', multer().single('upfile'), (req, res) => {
+  console.log(req.file)
+
+  var returnObject = {}
+  returnObject['name'] = req.file.originalname
+  returnObject['type'] = req.file.mimetype
+  returnObject['size'] = req.file.size
+  
+  res.json(returnObject)
+})
